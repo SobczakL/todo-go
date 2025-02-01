718f338422a71f12eafb54074cfa13f444ccf068
Lucas sobczak <lucassobczak3@gmail.com>
2025-02-01 00:33:21 -0500

    writing into csv

--- main.go
+++ main.go
@@ -3,47 +3,125 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
-func read(file *os.File) {
-	reader := csv.NewReader(file)
-
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal("Error reading record", err)
-		}
-
-		fmt.Println(record)
+func readAll(filename string) ([]map[string]string, error) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
 	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(rows) < 2 {
+		log.Fatal("CSV file is empty of only contains headers")
+	}
+
+	headers := rows[0]
+	var records []map[string]string
+
+	for _, row := range rows[1:] {
+		record := make(map[string]string)
+		for i, value := range row {
+			record[headers[i]] = value
+		}
+		records = append(records, record)
+	}
+	return records, nil
 }
 
-func main() {
-	firstTasks := [][]string{
-		{"task_number", "task_name"},
-		{"1", "walk dog"},
-	}
-
-	tasks, err := os.Open("tasks.csv")
+func writeNewTask(fileName string, newTask string) {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		read(tasks)
+		log.Fatal("Error opening file:", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal("Error reading file:", err)
 	}
 
-	w := csv.NewWriter(os.Stdout)
+	if len(rows) == 0 {
+		log.Fatal("CSV file is empty")
+	}
 
-	for _, task := range firstTasks {
-		if err := w.Write(task); err != nil {
-			log.Fatalln("error writing tasks")
+	var lastID int
+	if len(rows) > 1 {
+		lastRow := rows[len(rows)-1]
+		lastID, err = strconv.Atoi(lastRow[0])
+		if err != nil {
+			log.Fatal("Error converting last ID:", err)
 		}
+	} else {
+		lastID = 0
 	}
 
-	if err := w.Error(); err != nil {
-		log.Fatal(err)
+	newID := lastID + 1
+	file, err = os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("Error opening file for writing:", err)
+	}
+	defer file.Close()
+
+	newRow := []string{strconv.Itoa(newID), newTask}
+
+	writer := csv.NewWriter(file)
+	err = writer.Write(newRow)
+	if err != nil {
+		log.Fatal("Error writing to file:", err)
+	}
+	fmt.Println("New task added with ID:", newID)
+}
+
+//	func deleteTask(file *os.File, task int) {
+//		file.Seek(0, 0)
+//		reader := csv.NewReader(file)
+//
+//		for {
+//			record, err := reader.Read()
+//			if err == io.EOF {
+//				break
+//			}
+//			if err != nil {
+//				log.Fatal("Error reading file:", err)
+//			}
+//			fmt.Printf("%v", record)
+//		}
+//	}
+func main() {
+	fmt.Println("Hello, would you like to see tasks and create a new one?")
+	fmt.Println("1: read")
+	fmt.Println("2: write")
+	fmt.Println("3: delete task")
+
+	var option int
+	fmt.Scanf("%d", &option)
+
+	switch option {
+	case 1:
+		readAll("tasks.csv")
+	case 2:
+		fmt.Println("What would you like to write?")
+		var task string
+		fmt.Scanf("%v", &task)
+		writeNewTask("tasks.csv", task)
+	// case 3:
+	// 	readAll("tasks.csv")
+	// 	fmt.Println("Which task do you want deleted?")
+	// 	var task int
+	// 	fmt.Scanf("%d", task)
+	// 	deleteTask("tasks.csv", task)
+	default:
+		fmt.Println("Invalid Option")
 	}
 }
